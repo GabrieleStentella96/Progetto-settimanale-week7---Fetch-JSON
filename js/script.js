d4183ac368e3d7ddcb1c11370e20e97449c10d53
GabrieleStentella96 <104636389+GabrieleStentella96@users.noreply.github.com>
2022-06-11 18:19:46 +0200

    terzo commit

--- js/script.js
+++ js/script.js
@@ -21,14 +21,32 @@ fetch('json/users.json').then(response => {
         let userGender = document.createElement('p');
         userGender.innerHTML = `${element.gender}`;
 
+        let userSubj = document.createElement('p');
+        userSubj.innerHTML = `${element.subjet}`;
+
         let userEmail = document.createElement('p');
         userEmail.innerHTML = `Email: <span>${element.email}</span>`;
+
         userCard.appendChild(userId);
         userCard.appendChild(userImg);
         userCard.appendChild(userNickname);
         userCard.appendChild(userGender);
+        userCard.appendChild(userSubj);
         userCard.appendChild(userEmail);
 
         
      });
  };
+
+
+
+ ScrollReveal ({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+
+});
+
+ScrollReveal().reveal('h1', { delay: 300, origin: 'top' });
+ScrollReveal().reveal('div', { delay: 1200, origin: 'bottom', interval: 1200});
+
