d9933422c88117979442075417a32359b580e46c
GabrieleStentella96 <104636389+GabrieleStentella96@users.noreply.github.com>
2022-06-11 17:51:39 +0200

    secondo commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 fetch('json/users.json').then(response => {
     return response.json();
  })
- 
- .then(usersData => showCards(usersData));
+ .then(data => showCards(data));
  
  function showCards(users) {
-     let container = document.querySelector('#card');
+     let card = document.querySelector('#card');
      users.forEach(element => {
-         let card = document.createElement('div');
-         card.innerHTML =    `
-                             <h2>${element.firstName} ${element.lastName}</h2>
-                             <img src="${element.profileURL}" alt="">
-                             <li>Gender: ${element.gender}</li>
-                             <li>Username: ${element.username}</li>
-                             <li>Email: ${element.email}</li>
-                             `
-         container.appendChild(card);
+        let userCard = document.createElement('div');
+        card.appendChild(userCard);
+
+        let userId = document.createElement('h2');
+        userId.innerHTML = `${element.firstName} ${element.lastName}`;
+
+        let userImg = document.createElement('img');
+        userImg.src = `${element.profileURL}`;
+
+        let userNickname = document.createElement('p');
+        userNickname.innerHTML = `${element.username}`;
+
+        let userGender = document.createElement('p');
+        userGender.innerHTML = `${element.gender}`;
+
+        let userEmail = document.createElement('p');
+        userEmail.innerHTML = `Email: <span>${element.email}</span>`;
+        userCard.appendChild(userId);
+        userCard.appendChild(userImg);
+        userCard.appendChild(userNickname);
+        userCard.appendChild(userGender);
+        userCard.appendChild(userEmail);
+
+        
      });
  };
